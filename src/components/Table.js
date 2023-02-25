f1b032046e4805600501d9924d52a6cbd7389f42
Kimberly Blankenstein <kimberly.blankenstein@gmail.com>
2023-02-24 20:10:13 -0700

    Added new component called Table

---
+++ src/components/Table.js
@@ -0,0 +1,15 @@
+import './Table.css';
+
+function Table() {
+  return (
+    <div className="Table">
+      <header className="App-header">
+        <p>
+          This is where the table will go.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Table;
