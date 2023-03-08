803affdc494da988544f00e956dbc8c7114c0b8f
Kimberly Blankenstein <kimberly.blankenstein@gmail.com>
2023-03-07 19:53:13 -0700

    Added Leaflet map for testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function App() {
   return (
@@ -11,6 +12,17 @@ function App() {
         <h1>The Joe Noyes Crew</h1>
         <h4>May we never forget these ten airmen.</h4>
       </header>
+      <MapContainer center={[52.3113295, 1.2122331]} zoom={17} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[52.3113295, 1.2122331]}>
+          <Popup>
+            The 95th Bomb Group (H) was <br /> based at Horham, England.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
