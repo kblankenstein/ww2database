5fef52f79ba9b6c7e5183a20fcd961c72c9a94ac
Kimberly Blankenstein <kimberly.blankenstein@gmail.com>
2023-03-18 18:10:33 -0600

    Moving the table into App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,162 @@
 import "./App.css";
+import "ag-grid-community/styles//ag-grid.css";
+import "ag-grid-community/styles//ag-theme-alpine.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { MapContainer, TileLayer, Marker, Popup, LayerGroup, Circle } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  LayerGroup,
+  Circle,
+} from "react-leaflet";
+import React, { useState, useRef, useMemo, useCallback } from "react";
+import { AgGridReact } from "ag-grid-react";
 
 function App() {
+  const gridRef = useRef();
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState();
+  const headerHeight = 50;
+
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      field: "date",
+      headerTooltip: "Month, Day, Year",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+    {
+      field: "target",
+      headerTooltip: "Target",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+    {
+      field: "pilot",
+      headerTooltip: "Aircraft Commander",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+    {
+      field: "copilot",
+      headerTooltip: "Co-Pilot",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+    {
+      field: "aircraft",
+      headerTooltip: "B-17 Serial Number",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+    {
+      field: "name",
+      headerTooltip: "Nose Art / Name",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+    {
+      field: "squadron",
+      headerTooltip: "Bomb Squadron",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+    {
+      field: "coordinates",
+      headerTooltip: "Coordinates",
+      width: 200,
+      minWidth: 200,
+      maxWidth: 220,
+    },
+  ]);
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    flex: 1,
+  }));
+
+  const onGridReady = useCallback((params) => {
+    fetch("https://apilotnamedjoe.com/data.json")
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
+
+  const onFirstDataRendered = useCallback((params) => {
+    gridRef.current.api.sizeColumnsToFit();
+  }, []);
+
+  const onSelectionChanged = useCallback(() => {
+    const targetName = gridRef.current.api.getSelectedRows();
+    document.querySelector("#targetName").innerHTML =
+      targetName.length === 1 ? targetName[0].target : "";
+  }, []);
+
+  const mapCoords = null;
+  //add logic to change marker position based on table row selection
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The Joe Noyes Crew</h1>
-        <h4>May we never forget these ten airmen.</h4>
-      </header>
-      <MapContainer center={[52.3113295, 1.2122331]} zoom={14} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[52.3113295, 1.2122331]}>
-          <Popup>
-            The 95th Bomb Group (H) was <br /> based at Horham, England.
-          </Popup>
-        </Marker>
-        <LayerGroup>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>The Joe Noyes Crew</h1>
+          <h4>May we never forget these ten airmen.</h4>
+        </header>
+        <MapContainer
+          center={[52.3113295, 1.2122331]}
+          zoom={14}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[52.3113295, 1.2122331]}>
+            <Popup>
+              The 95th Bomb Group (H) was <br /> based at Horham, England.
+            </Popup>
+          </Marker>
+          <LayerGroup>
             <Circle
               center={[52.3113295, 1.2122331]}
-              pathOptions={{ color: 'green', fillColor: 'green' }}
+              pathOptions={{ color: "green", fillColor: "green" }}
               radius={200}
             />
           </LayerGroup>
-      </MapContainer>
-    </div>
+        </MapContainer>
+      </div>
+      <div style={containerStyle}>
+        <div className="ag-theme-alpine" style={gridStyle}>
+          <div className="targetName">
+            <span id="targetName"></span>
+          </div>
+          <AgGridReact
+            ref={gridRef}
+            rowData={rowData}
+            columnDefs={columnDefs}
+            defaultColDef={defaultColDef}
+            onGridReady={onGridReady}
+            animateRows={true}
+            headerHeight={headerHeight}
+            onFirstDataRendered={onFirstDataRendered}
+            domLayout="autoHeight"
+            rowSelection={"single"}
+            onSelectionChanged={onSelectionChanged}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,5 @@
 import "./Main.css";
 import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Intro from "./Intro";
-import Joe from "./Joe";
-import Table from "./Table";
-import Crew from "./Crew";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import EmailIcon from "@mui/icons-material/Email";
 import Facebook from "@mui/icons-material/Facebook";
@@ -16,65 +7,10 @@ import Button from "@mui/material/Button";
 import "ag-grid-community/styles//ag-grid.css";
 import "ag-grid-community/styles//ag-theme-alpine.css";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
 const Main = () => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
   return (
     <>
-      <div className="tabs">
-        <Box sx={{ width: "100%", flexWrap: "wrap" }}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="Tabbed page navigation"
-              centered
-            >
-              <Tab label="Mission List" {...a11yProps(0)} />
-            </Tabs>
-          </Box>
-          <TabPanel value={value} index={0}>
-            <Table />
-          </TabPanel>
-        </Box>
-      </div>
       <div className="contact">
         <Button
           variant="outlined"
